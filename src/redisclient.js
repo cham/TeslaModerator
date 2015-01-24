89ccaad92862168495c82276868b17d0037366e3
Dan Neame <danneame@gmail.com>
2015-01-24 16:52:04 +0000

    redisclient logic

---
+++ src/redisclient.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Q = require('q');
+var redis = require('redis');
+var client = redis.createClient();
+var deferred = Q.defer();
+
+client.once('error', function(err){
+    console.error('redis connection error: ' + err);
+    deferred.reject();
+});
+
+client.once('ready', function(){
+    console.log('redis connected');
+    deferred.resolve();
+});
+
+exports.getClient = function(callback){
+    deferred.promise.then(function(){
+        callback(client);
+    });
+};
