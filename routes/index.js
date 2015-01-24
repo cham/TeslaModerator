fb6f9e021915fd2e927e4af50ba755a5ac4f0485
Dan Neame <danneame@gmail.com>
2015-01-24 20:55:59 +0000

    index logic for login POST route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 'use strict';
 
+var authentication = require('../src/api/authentication');
 var index = {};
 
 index.get = function(req, res){
     res.sendStatus(200);
 };
 
+function login(req, res){
+    authentication.login(req, {
+        username: req.body.username,
+        password: req.body.password
+    }, function(err){
+        if(err){
+            res.status(401);
+            return res.send(err.message);
+        }
+
+        res.sendStatus(200);
+    });
+}
+
 module.exports = function(router){
-    router.get('/', index.get);
+    router
+        .get('/', index.get)
+        .post('/login', login);
 };
