5b43943fa3f3026f35c5d36018cc0b1a6841b2a9
Dan Neame <danneame@gmail.com>
2015-01-24 16:25:39 +0000

    remove redis requirement from moderator-app

--- moderator-app.js
+++ moderator-app.js
@@ -3,25 +3,15 @@
 var express = require('express');
 var routemaster = require('routemaster');
 var http = require('http');
-var redis = require('redis');
 
 var port = 3030;
 var app = express();
 var server = http.createServer(app);
-var client = redis.createClient();
 
-client.on('error', function(err){
-    console.error('redis client error: ' + err);
-});
+app.use(routemaster({
+    directory: 'routes',
+    Router: express.Router
+}));
 
-client.once('ready', function(){
-    console.log('redis connected');
-
-    app.use(routemaster({
-        directory: 'routes',
-        Router: express.Router
-    }));
-
-    server.listen(port);
-    console.log('moderator-app listening on port', port);
-});
+server.listen(port);
+console.log('moderator-app listening on port', port);
