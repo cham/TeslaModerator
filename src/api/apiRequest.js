7e862d6e930353c799794f52f7b68e16c380c33a
Dan Neame <danneame@gmail.com>
2015-01-24 20:36:59 +0000

    apiRequest logic

---
+++ src/api/apiRequest.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var request = require('request').defaults({
+    encoding: 'utf8',
+    json: true,
+    gzip: true
+});
+
+var errorCodes = [500, 401, 403, 404];
+var errorCodeToTextMap = {
+    401: 'Unauthorised',
+    403: 'Forbidden',
+    404: 'Route not found',
+    500: 'API error'
+};
+
+function checkResponse(err, apiresponse, callback){
+    if(err){
+        return callback(err);
+    }
+
+    if(apiresponse && errorCodes.indexOf(apiresponse.statusCode) > -1){
+        callback(new Error(apiresponse.body || errorCodeToTextMap[apiresponse.statusCode]));
+        return false;
+    }
+
+    return true;
+}
+
+function parseJson(json, callback){
+    try{
+        json = JSON.parse(json);
+    }catch(e){
+        console.log(e, json);
+        return callback({});
+    }
+    callback(json);
+}
+
+function requiredOptions(options){
+    if(!options.method){
+        throw new Error('method is required');
+    }
+    if(!options.url){
+        throw new Error('url is required');
+    }
+}
+
+function makeRequest(options, callback){
+    requiredOptions(options || {});
+
+    request(options, function(err, response, json){
+        if(!checkResponse(err, response, callback)){
+            return;
+        }
+
+        parseJson(json, function(data){
+            if(callback){
+                callback(null, data);
+            }
+        });
+    });
+}
+
+exports.makeRequest = makeRequest;
