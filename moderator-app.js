feafc84be78930001cbbc8be954dcde93bbd19e6
Dan Neame <danneame@gmail.com>
2015-01-24 16:03:20 +0000

    require redis ready before starting app

--- moderator-app.js
+++ moderator-app.js
@@ -3,15 +3,25 @@
 var express = require('express');
 var routemaster = require('routemaster');
 var http = require('http');
+var redis = require('redis');
 
 var port = 3030;
 var app = express();
 var server = http.createServer(app);
+var client = redis.createClient();
 
-app.use(routemaster({
-    directory: 'routes',
-    Router: express.Router
-}));
+client.on('error', function(err){
+    console.error('redis client error: ' + err);
+});
 
-server.listen(port);
-console.log('moderator-app listening on port', port);
+client.once('ready', function(){
+    console.log('redis connected');
+
+    app.use(routemaster({
+        directory: 'routes',
+        Router: express.Router
+    }));
+
+    server.listen(port);
+    console.log('moderator-app listening on port', port);
+});
