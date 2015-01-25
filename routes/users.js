1805096da6fa442fc32a020c5dcff76ed89f1423
Dan Neame <danneame@gmail.com>
2015-01-25 12:23:59 +0000

    routes for search and edit users

--- routes/users.js
+++ routes/users.js
@@ -23,12 +23,38 @@ userRouteHandlers.changePassword = function(req, res){
         if(err){
             return res.sendStatus(400);
         }
-        return res.sendStatus(200);
+        res.sendStatus(200);
+    });
+};
+
+userRouteHandlers.getUsers = function(req, res){
+    users.getUsers(req.query || {}, null, function(err, json){
+        if(err){
+            return res.sendStatus(400);
+        }
+        res.render('users', json);
+    });
+};
+
+userRouteHandlers.editUser = function(req, res){
+    var body = req.body;
+
+    users.editUser(body.username, {
+        email: body.email,
+        points: body.points,
+        banned: body.banned === 'on'
+    }, function(err){
+        if(err){
+            return res.sendStatus(400);
+        }
+        res.redirect('/users?startswith=' + body.username);
     });
 };
 
 module.exports = function(router){
     router
         .get('/change-password', requireAuthentication, userRouteHandlers.renderChangePassword)
-        .post('/change-password', requireAuthentication, userRouteHandlers.changePassword);
+        .post('/change-password', requireAuthentication, userRouteHandlers.changePassword)
+        .get('/users', requireAuthentication, userRouteHandlers.getUsers)
+        .post('/user', requireAuthentication, userRouteHandlers.editUser);
 };
